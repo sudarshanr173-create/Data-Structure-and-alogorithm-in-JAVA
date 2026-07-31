66c52024ba1ad749e4218175c938d1c157f90012
sudarshanr173-create <sudarshanr173@gmail.com>
2026-08-01 00:17:28 +0530

    Add files via upload

---
+++ greedyalogrithm2.java
@@ -0,0 +1,16 @@
+public class greedyalogrithm2 {
+    public static void main(String args[]) {
+        int A[] = { 1, 2, 3 };
+        int B[] = { 2, 1, 3 };
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+        System.out.println("Minimum Difference = " + minDiff);
+    }
+}
